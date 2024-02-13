313adfe7400b4713c96f1ccce88cc98ecfa32f08
madubhan <madubhan@yahoo.com>
2024-02-13 14:45:32 -0500

    committing code and example

---
+++ example.py
@@ -0,0 +1,29 @@
+#Example 1
+# name = "Bro"
+# age = 21
+# attractive = True
+# Same code as above and below
+# name, age, attractive = "Bro", 21, True
+#
+# print(name)
+# print(age)
+# print(attractive)
+
+#Example 2
+
+# Spongebob = 30
+# Patrick = 30
+# Sandy = 30
+# Squidward = 30
+
+ # or you can write them in one line of code
+
+Spongebob = Patrick = Sandy = Squidward = 30
+
+print(Spongebob)
+print(Patrick)
+print(Sandy)
+print(Squidward)
+
+
+
